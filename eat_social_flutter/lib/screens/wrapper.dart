ae155dd9a222e484a49183372fe5a278bbb31797
DevangJayswal <dnjayswal@gmail.com>
2020-07-30 16:19:33 -0400

    added annonymous signIn
    changed minsdk version 16 to 21 to work with firebase
    check android->app->build.gradle

---
+++ eat_social_flutter/lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:eat_social_flutter/screens/authenticate/authenticate.dart';
+// import 'home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return either the Home or Authenticate widget
+    return Authenticate();
+  }
+}
